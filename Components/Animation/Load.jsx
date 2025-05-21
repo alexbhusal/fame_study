a44b91faf489e5642ca56b76c06913752aaeaf0d
Alex <alexbhusal@icloud.com>
2025-05-21 18:01:08 +0545

    first commit

---
+++ Components/Animation/Load.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const Load = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/9795c428-e24b-428f-90f6-9195650f95de/cBVx8QE4BR.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+export default Load;
---
+++ Components/Animation/UnderDevlop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const Under = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/ad088095-0ca8-4586-a066-45b8dcb88e6a/spUBFXPCV2.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+export default Under;
